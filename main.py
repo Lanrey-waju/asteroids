813a0cec16747fa7001f19d79f2bc16ab6ff6344
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-11-28 21:33:03 +0100

    feat: add and render player on the screen

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
-import constants
+from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player1 = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+
+        delta = clock.tick(50)
+        dt = delta / 1000
 
 
 if __name__ == "__main__":
